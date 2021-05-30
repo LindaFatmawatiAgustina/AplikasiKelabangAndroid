1c3f517dd7ea78d0fd56e981a6dc8608eddc6e1d
LindaFatmawatiAgustina <lindawatiagustina132@gmail.com>
2021-05-30 18:50:04 +0700

    Push and SPlashcreen kelabang

---
+++ app/src/main/java/com/aer/kelabangapp/SplashScreen.kt
@@ -0,0 +1,25 @@
+package com.aer.kelabangapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.Window
+import android.view.WindowManager
+import org.jetbrains.anko.startActivity
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        //4second splash time
+        Handler().postDelayed({
+            //start main activity
+            startActivity(Intent(this, MainActivity::class.java))
+            //finish this activity
+            finish()
+        },3000)
+
+    }
+    }
