b537cc008dba450c30f438f6bae20a2d840ce73e
LindaFatmawatiAgustina <lindawatiagustina132@gmail.com>
2021-06-03 21:59:50 +0700

    membuat lihat laporan dan tambah laporan xml

---
+++ app/src/main/java/com/aer/kelabangapp/Tambah_laporan_jalan.kt
@@ -0,0 +1,11 @@
+package com.aer.kelabangapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Tambah_laporan_jalan : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tambah_laporan_jalan)
+    }
+}
